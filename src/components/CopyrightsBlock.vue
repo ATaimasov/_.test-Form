465d91fe38bf65d71ffce67879684ef33964eaf6
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2025-02-28 13:15:47 +0300

    update logo

---
+++ src/components/CopyrightsBlock.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import CopyrightLogo from "@/components/icons/CopyrightLogo.vue";
+</script>
+
+<template>
+  <div class="copyrights">
+    <a
+      href="https://github.com/ATaimasov"
+      target="_blank"
+      class="copyrights__link"
+    >
+      <CopyrightLogo class="logo copyrights__logo" />
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.copyrights {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.copyrights__link {
+  color: black;
+  text-decoration: none;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.7;
+  }
+}
+
+.copyrights__logo {
+  scale: 0.4;
+
+  @media screen and (min-width: 750px) {
+    scale: 0.8;
+  }
+  @media screen and (min-width: 1100px) {
+    scale: 1;
+  }
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="#header">
     <div class="header-container">
-      <CompanyIcon class="header__logo"></CompanyIcon>
+      <CopyrightsBlock class="header__logo" />
       <ul class="header__list">
         <router-link to="/form" class="link">
           <li class="header__list-item">Форма</li>
@@ -15,7 +15,7 @@
 </template>
 
 <script setup>
-import CompanyIcon from "@/components/icons/CompanyIcon.vue";
+import CopyrightsBlock from "@/components/CopyrightsBlock.vue";
 </script>
 
 <style lang="scss">
@@ -23,30 +23,30 @@ import CompanyIcon from "@/components/icons/CompanyIcon.vue";
   height: 100%;
   width: 100%;
   box-shadow: 0px 1px 0px rgba(17, 17, 17, 0.1);
+}
 
-  &-container {
-    display: grid;
-    grid-template-columns: 50% 50%;
-    align-items: center;
-    align-content: center;
-    height: 100%;
-    width: 100%;
+.header-container {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: center;
+  align-content: center;
+  height: 100%;
+  width: 100%;
+}
+
+.header__list {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1.5rem; //24px;
+
+  &-item {
+    font-size: 1rem;
+    line-height: 1.5rem;
   }
+}
 
-  &__list {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 1.5rem; //24px;
-
-    &-item {
-      font-size: 1rem;
-      line-height: 1.5rem;
-    }
-  }
-
-  &__logo {
-    margin-left: 6.64%; //51px
-  }
+.header__logo {
+  margin-left: 6.64%; //51px
 }
 </style>
